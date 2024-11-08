83fc8c3a04efbaa9103da284d762b55a75473512
anila <anila@GokulamPC>
2024-11-08 19:38:37 +0530

    commit

---
+++ firstjavaproject/src/samplepackage/HelloWorld.java
@@ -0,0 +1,17 @@
+package samplepackage;
+public class HelloWorld {
+
+
+	public void display()
+	{
+	System.out.println("Hello world");
+	}
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			HelloWorld obj = new HelloWorld();
+			obj.display();
+
+		}
+}
